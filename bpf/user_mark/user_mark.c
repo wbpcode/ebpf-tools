c084c0340a696b075ee6eaac203d952a55143b28
wbpcode <wbphub@live.com>
2024-01-15 18:11:37 +0800

    ignore sock that created by proxy
    Signed-off-by: wbpcode <wbphub@live.com>

--- bpf/user_mark/user_mark.c
+++ bpf/user_mark/user_mark.c
@@ -5,11 +5,17 @@
 #include <asm/socket.h>
 #include <bpf/bpf_helpers.h>
 
+#define PROXY_UID 0
+
 SEC("cgroup/sock_create")
 int set_user_id_as_so_mark(struct bpf_sock *ctx)
 {
     __u32 uid = bpf_get_current_uid_gid();
-    ctx->mark = uid;
+
+    if (uid != PROXY_UID)
+    {
+        ctx->mark = uid;
+    }
     return 1;
 }
 
